7e41b5c42d398756657f76f366bf58ebe59e944d
Nitish Bangera <bangera.nitish@gmail.com>
2020-12-08 16:55:12 +0530

    refactor: Adding method to produce a message into Kafka.

--- src/queue/queue.go
+++ src/queue/queue.go
@@ -9,35 +9,57 @@ import (
 	"github.com/segmentio/kafka-go"
 )
 
+// Queue Structure
 type Queue struct {
 	context context.Context
-	topic   string
-	brokers []string
-	groupId string
+	reader  *kafka.Reader
+	writer  *kafka.Writer
 }
 
-func New(topic string, brokers []string, groupId string) *Queue {
-	return &Queue{context: context.Background(), topic: topic, brokers: brokers, groupId: groupId}
-}
-
-func (queue *Queue) Consume() {
-	r := kafka.NewReader(kafka.ReaderConfig{
-		Brokers:     queue.brokers,
-		Topic:       queue.topic,
-		GroupID:     queue.groupId,
+// New method creates a Queue object.
+func New(topic string, brokers []string, groupID string) *Queue {
+	reader := kafka.NewReader(kafka.ReaderConfig{
+		Brokers:     brokers,
+		Topic:       topic,
+		GroupID:     groupID,
 		StartOffset: kafka.LastOffset,
 	})
+
+	writer := kafka.NewWriter(kafka.WriterConfig{
+		Brokers: brokers,
+		Topic:   topic,
+	})
+	return &Queue{context: context.Background(), reader: reader, writer: writer}
+}
+
+// Consume method creates a reader from Kafka and reads the messages.
+func (queue *Queue) Consume() {
 	for {
 		// the `ReadMessage` method blocks until we receive the next event
-		msg, err := r.ReadMessage(queue.context)
+		msg, err := queue.reader.ReadMessage(queue.context)
 		if err != nil {
 			panic("Could not read message " + err.Error())
 		}
 		var eventNotif model.Eventnotification
 		if err := json.Unmarshal(msg.Value, &eventNotif); err != nil {
-			fmt.Println("failed to unmarshal:", err)
+			fmt.Println("Failed to unmarshal:", err)
 		} else {
-			fmt.Println(eventNotif)
+			fmt.Println("Received :", eventNotif)
+		}
+	}
+}
+
+// Produce method produces a message to kafka
+func (queue *Queue) Produce(eventNotification model.Eventnotification) {
+	data, err := json.Marshal(eventNotification)
+	if err != nil {
+		panic("Couldn't marshal data : " + err.Error())
+	} else {
+		err := queue.writer.WriteMessages(queue.context, kafka.Message{
+			Value: data,
+		})
+		if err != nil {
+			panic("Couldn't write message " + err.Error())
 		}
 	}
 }
